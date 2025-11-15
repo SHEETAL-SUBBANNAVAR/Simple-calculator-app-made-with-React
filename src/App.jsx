d4b4b73177e0e0a9af926c44b6c15cba98c22d77
sheetal subbannavar <sheetalssubbbu@gmail.com>
2025-11-15 22:32:59 +0530

    your message

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import Display from "./components/Display";
 import ButtonsContainer from "./components/ButtonsContainer";
 import styles from "./App.module.css";
+import { useState } from "react";
+
 
 function App() {
+  const [calVal , setCalVal] = useState("");
+  const onButtonClick =(buttonText)=>{
+    if(buttonText === 'C'){
+
+      setCalVal("");
+    }
+    else if(buttonText === '='){
+      try {
+        setCalVal(eval(calVal).toString());
+      } catch {
+        setCalVal("Error");
+     }
+    }
+
+    else{
+      const newDisplayValue =calVal + buttonText;
+      setCalVal(newDisplayValue)
+    }                                
+  }
   return (
     <div className={styles.calculator}>
-      <Display />
-      <ButtonsContainer />
+      <Display DisplayValue={calVal}></Display>
+      <ButtonsContainer onButtonClick={onButtonClick} ></ButtonsContainer>
     </div>
   );
 }
 
-export default App;
+export default App; 
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,7 +1,7 @@
 import styles from "./Display.module.css";
 
-const Display = () => {
-  return <input type="text" className={styles.display} readOnly />;
+const Display = ({DisplayValue}) => {
+  return <input type="text" className={styles.display} value={DisplayValue} readOnly></input>;
 };
 
 export default Display;
